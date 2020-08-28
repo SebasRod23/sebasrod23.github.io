c1e1d102394625976333ded5499b8d628e54b956
SebasRod23 <sebastian_rodriguezg@yahoo.com>
2020-08-27 22:54:33 -0500

    First commit
    Addes header, hashrouter from react-router-dom, and first draft for design

---
+++ precache-manifest.7843b823ee7dac245443d29b7abb8f82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6b713612d60f3842c05ecc3acfaacf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9073b195e6daf93c2b95",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "064498b43fdfb96845a5",
+    "url": "/static/css/main.2ea7a381.chunk.css"
+  },
+  {
+    "revision": "9073b195e6daf93c2b95",
+    "url": "/static/js/2.9ba63b34.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.9ba63b34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "064498b43fdfb96845a5",
+    "url": "/static/js/main.24b51e66.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
